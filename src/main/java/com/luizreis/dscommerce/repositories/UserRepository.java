49a42e02a2f64b522c306533a2ca2f4ed3797552
Luiz Reis <reisluiz05@gmail.com>
2023-09-10 08:04:11 -0300

    GET user logged

--- src/main/java/com/luizreis/dscommerce/repositories/UserRepository.java
+++ src/main/java/com/luizreis/dscommerce/repositories/UserRepository.java
@@ -1,6 +1,7 @@
 package com.luizreis.dscommerce.repositories;
 
 import java.util.List;
+import java.util.Optional;
 
 import com.luizreis.dscommerce.entities.User;
 import com.luizreis.dscommerce.projections.UserDetailsProjection;
@@ -20,4 +21,6 @@ public interface UserRepository extends JpaRepository<User, Long> {
 				WHERE tb_user.email = :email
 			""")
     List<UserDetailsProjection> searchUserAndRolesByEmail(String email);
+
+	Optional<User> findByEmail(String email);
 }
--- src/main/java/com/luizreis/dscommerce/services/UserService.java
+++ src/main/java/com/luizreis/dscommerce/services/UserService.java
@@ -2,15 +2,20 @@ package com.luizreis.dscommerce.services;
 
 import java.util.List;
 
+import com.luizreis.dscommerce.dto.UserDTO;
 import com.luizreis.dscommerce.entities.Role;
 import com.luizreis.dscommerce.entities.User;
 import com.luizreis.dscommerce.projections.UserDetailsProjection;
 import com.luizreis.dscommerce.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 
 @Service
@@ -35,5 +40,22 @@ public class UserService implements UserDetailsService {
         }
 
         return user;
+    }
+
+    protected User authenticated(){
+        try {
+            Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+            Jwt jwtPrincipal = (Jwt) authentication.getPrincipal();
+            String username = jwtPrincipal.getClaim("username");
+            return repository.findByEmail(username).get();
+        }catch (Exception e){
+            throw new UsernameNotFoundException("Email not found");
+        }
+    }
+
+    @Transactional(readOnly = true)
+    public UserDTO getMe(){
+        User user = authenticated();
+        return new UserDTO(user);
     }
 }
